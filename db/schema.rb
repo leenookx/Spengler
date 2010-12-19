9b929a4a0cf8de8d717d5865087160d9868ea78d
Lee Nookx <lnookx@googlemail.com>
2010-12-19 12:13:42 +0000

    Add a new status field to users.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101219113857) do
+ActiveRecord::Schema.define(:version => 20101219121118) do
 
   create_table "activations", :id => false, :force => true do |t|
     t.string   "code",       :null => false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20101219113857) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "hashed_password"
+    t.integer  "status",          :default => 0
   end
 
 end

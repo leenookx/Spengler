f126d9848fa9f27d9eaa674de9ccd85218bcf7ae
lnookx <lnookx@googlemail.com>
2011-01-02 12:21:21 +0000

    Don't bother trying to delete all of the data first.

--- db/migrate/20101219091158_load_system_status_data.rb
+++ db/migrate/20101219091158_load_system_status_data.rb
@@ -2,7 +2,6 @@ require 'active_record/fixtures'
 
 class LoadSystemStatusData < ActiveRecord::Migration
   def self.up
-    down
     directory = File.join(File.dirname(__FILE__), "data")
     Fixtures.create_fixtures(directory, "system_statuses")
   end

e2a9642e2c66a9a0f72c7889e537db441fbd1556
lnookx <lnookx@googlemail.com>
2011-01-09 14:06:54 +0000

    Try and freeze our gem dependencies.

--- config/environment.rb
+++ config/environment.rb
@@ -18,6 +18,7 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
 
   config.gem "daemons"
+  config.gem "rack", :version => "1.1.0"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
